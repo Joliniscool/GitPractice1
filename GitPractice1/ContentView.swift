d9944942cf00f3a8166eefc7afc2e32382146ebb
JOJO <jolinwangt@gmail.com>
2025-07-23 11:45:24 -0700

    change 3

--- GitPractice1/ContentView.swift
+++ GitPractice1/ContentView.swift
@@ -10,6 +10,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Hello World!")
+            Text("More practice saving")
         }
         .padding()
     }
