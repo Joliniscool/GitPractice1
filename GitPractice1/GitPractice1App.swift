388cf19501ccdbaa044b82c04b9a55bca70e2824
JOJO <jolinwangt@gmail.com>
2025-07-23 11:03:32 -0700

    Initial Commit

---
+++ GitPractice1/GitPractice1App.swift
@@ -0,0 +1,32 @@
+//
+//  GitPractice1App.swift
+//  GitPractice1
+//
+//  Created by Jolin Wang on 7/23/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GitPractice1App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ GitPractice1Tests/GitPractice1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  GitPractice1Tests.swift
+//  GitPractice1Tests
+//
+//  Created by Jolin Wang on 7/23/25.
+//
+
+import Testing
+@testable import GitPractice1
+
+struct GitPractice1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
